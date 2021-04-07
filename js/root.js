98fe5ee2fd3380ea60529bcaa05644e698acb196
Stefano Tosatto <stefanotosatto.work@gmail.com>
2021-04-07 15:58:34 +0200

    milestones 2, 3 e 1/2

--- js/root.js
+++ js/root.js
@@ -3,8 +3,8 @@ var app = new Vue({
 
     data: {
         searchFilm: '',
-        films: []
-
+        films: [],
+        apiK: '5f03ee3b0c28f08b12f985fc9e4ae8c8'
     },
 
     mounted() {
@@ -14,13 +14,36 @@ var app = new Vue({
 
     methods: {
         getFilms(){
+            this.films = [];
             axios
-                .get("https://api.themoviedb.org/3/search/movie?api_key=5f03ee3b0c28f08b12f985fc9e4ae8c8&language=it-IT&query=" + this.searchFilm)
+                .get('https://api.themoviedb.org/3/search/multi', {params: {
+                    api_key: this.apiK, query: this.searchFilm
+                }})
                 .then((response) => {
                     console.log(response);
-                    this.films = response.data.results;
+                    let tmp = response.data.results;
+                    tmp.forEach(element => {
+                        if (element.media_type == "movie" || element.media_type == "tv")
+                            this.films.push(element);
+                    });
                     console.log(this.films);
                 });
+        },
+
+        getFlag(index){
+            let flag = 'https://www.unknown.nu/flags/images/' + this.films[index].original_language + '-100';
+            return flag;
+        },
+
+        getVote(index){
+            let vote = Math.ceil(this.films[index].vote_average/2);
+            return vote;
+        },
+        
+        getPoster(index){
+            const urlPre = 'https://image.tmdb.org/t/p/';
+            let poster = urlPre + 'w342' + this.films[index].poster_path;
+            return poster;
         }
     }
 });
