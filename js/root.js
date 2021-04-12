1cf66853b96d024c9221b97d046fb2832b1f6327
Stefano Tosatto <stefanotosatto.work@gmail.com>
2021-04-12 12:27:32 +0200

    milestone 4

--- js/root.js
+++ js/root.js
@@ -4,7 +4,8 @@ var app = new Vue({
     data: {
         searchFilm: '',
         films: [],
-        apiK: '5f03ee3b0c28f08b12f985fc9e4ae8c8'
+        apiK: '5f03ee3b0c28f08b12f985fc9e4ae8c8',
+
     },
 
     mounted() {
@@ -13,17 +14,20 @@ var app = new Vue({
     },
 
     methods: {
-        getFilms(){
+        getFilms(searchString){
             this.films = [];
             axios
-                .get('https://api.themoviedb.org/3/search/multi', {params: {
-                    api_key: this.apiK, query: this.searchFilm
-                }})
+                .get('https://api.themoviedb.org/3/search/multi', {
+                    params: {
+                        api_key: this.apiK, 
+                        query: searchString
+                    }
+                })
                 .then((response) => {
                     console.log(response);
                     let tmp = response.data.results;
                     tmp.forEach(element => {
-                        if (element.media_type == "movie" || element.media_type == "tv")
+                        if (element.media_type != "person")
                             this.films.push(element);
                     });
                     console.log(this.films);
@@ -41,9 +45,39 @@ var app = new Vue({
         },
         
         getPoster(index){
-            const urlPre = 'https://image.tmdb.org/t/p/';
-            let poster = urlPre + 'w342' + this.films[index].poster_path;
-            return poster;
+            if (this.films[index].poster_path){
+                const urlPre = 'https://image.tmdb.org/t/p/';
+                let poster = urlPre + 'w342' + this.films[index].poster_path;
+                return poster;
+            } else {
+                return `img/noimage.png`
+            }
+        },
+
+        getGenres(index){
+
+        },
+
+        getActors(index){
+            let id = this.films[index].id;
+            let filmActors = [];
+            axios
+                .get('https://api.themoviedb.org/3/movie/' +  id + '/credits', {
+                    params: {
+                        api_key: this.apiK
+                    }
+                })
+                .then((response) => {
+                    console.log(response);
+                    let tmp = response.data.results;
+                    tmp.forEach(element => {
+                        if (i < 5)
+                            filmActors.push(element);
+                        i++;
+                    });
+                    console.log(filmActors);
+                });
+            return filmActors;
         }
     }
 });
